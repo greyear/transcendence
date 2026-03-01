594d99123735f5512cf07b188cdbbaf8750d3365
Nick <kolya59264@gmail.com>
2026-03-02 00:15:00 +0200

    Setup mobile card. Fix semantics.

--- frontend/src/components/cards/RecipeCard.tsx
+++ frontend/src/components/cards/RecipeCard.tsx
@@ -1,15 +1,22 @@
-import '../../styles/recipeCard.css'
+import '../../assets/styles/recipeCard.css'
+import recipeImg from '../../assets/images/vegetable-side-dishes.jpg'
 
 export const RecipeCard = () => {
 	return (
-		<div className='recipe-card'>
-			<img className='recipe-card-image'></img>
+		<article className='recipe-card'>
+			<img className='recipe-card-image' src={recipeImg} alt='Vegetable side dishes'></img>
 			<div className='recipe-card-container'>
 				<div className='recipe-card-content-column'>
-					<p className='text-label'>Title</p>
-					<p className='text-caption-s'>Short description</p>
+					<header className='recipe-card-title-description-column'>
+						<h3 className='text-label'>Title</h3>
+						<p className='text-caption-s'>Short description</p>
+					</header>
+					<footer className='recipe-card-tag-favorite-row'>
+						<span className='text-caption'>smth</span>
+						<button aria-label='Add to favorites'>heart</button>
+					</footer>
 				</div>
 			</div>
-		</div>
+		</article>
 	)
 }
