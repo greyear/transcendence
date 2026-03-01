58b2a795c5098c8308e3a00af74e67cbbdf1b1f8
SeelaSalorinta <seela.salorinta@gmail.com>
2026-03-01 09:43:07 +0200

    setup typography foundation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "@fontsource/glory/300.css";
+import "@fontsource/glory/400.css";
+import "@fontsource/glory/500.css";
+import "@fontsource/glory/700.css";
 import './index.css'
 import App from './App.tsx'
 
