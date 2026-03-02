695f14f3dfdcd5b31261d0b7f117f7e6464272ac
Nick <kolya59264@gmail.com>
2026-03-03 00:20:27 +0200

    Create UserCard mobile layout.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
 import { RecipeCard } from "./components/cards/RecipeCard.js"
+import { UserCard } from "./components/cards/UserCard.js"
 
 function App() {
-	return <RecipeCard />
+	return <>
+		<RecipeCard />
+		<UserCard />
+	</>
 }
 
 export default App
---
+++ frontend/src/components/cards/UserCard.tsx
@@ -0,0 +1,17 @@
+import userPhoto from '../../assets/images/user-photo.jpg'
+import '../../assets/styles/userCard.css'
+
+export const UserCard = () => {
+	return (
+		<article className='user-card'>
+			<img className='user-card-photo' src={userPhoto} alt='User profile photo' />
+			<div className='user-card-container'>
+				<header className='user-card-header'>
+					<h3>Name</h3>
+					<p className='text-body3'>123 recipes</p>
+				</header>
+				<button>Follow</button>
+			</div>
+		</article>
+	)
+}
