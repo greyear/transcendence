2ec2c1157b695e48b78331f4d4a0ec5329407dae
Nick Saveliev <msavelie@student.hive.fi>
2026-03-04 21:31:33 +0200

    Add adaptive.

--- frontend/src/components/cards/UserCard.tsx
+++ frontend/src/components/cards/UserCard.tsx
@@ -3,15 +3,17 @@ import '../../assets/styles/userCard.css'
 
 export const UserCard = () => {
 	return (
-		<article className='user-card'>
-			<img className='user-card-photo' src={userPhoto} alt='User profile photo' />
-			<div className='user-card-container'>
-				<header className='user-card-header'>
-					<h3>Name</h3>
-					<p className='text-body3'>123 recipes</p>
-				</header>
-				<button>Follow</button>
-			</div>
-		</article>
+		<a href='/user' className='user-card-link-wrapper'>
+			<article className='user-card'>
+				<img className='user-card-photo' src={userPhoto} alt='User profile photo' />
+				<div className='user-card-container'>
+					<header className='user-card-header'>
+						<h3>Name</h3>
+						<p className='text-body3'>123 recipes</p>
+					</header>
+					<button>Follow</button>
+				</div>
+			</article>
+		</a>
 	)
 }
