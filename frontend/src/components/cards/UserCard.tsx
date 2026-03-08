209607878307ac8d873336c853bff78a4f3e09fc
Nick <kolya59264@gmail.com>
2026-03-08 19:12:31 +0200

    Fix lint and formatting issues.

--- frontend/src/components/cards/UserCard.tsx
+++ frontend/src/components/cards/UserCard.tsx
@@ -1,30 +1,33 @@
-import { useState } from 'react'
-import userPhoto from '../../assets/images/user-photo.jpg'
-import '../../assets/styles/userCard.css'
-import { MainButton } from '../buttons/MainButton'
+import { useState } from "react";
+import userPhoto from "../../assets/images/user-photo.jpg";
+import "../../assets/styles/userCard.css";
+import { MainButton } from "../buttons/MainButton";
 
 export const UserCard = () => {
-	const [isActive, setIsActive] = useState(false)
+	const [isActive, setIsActive] = useState(false);
 	const onClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
 		e.stopPropagation();
 		e.preventDefault();
 		//TODO: add confirmation unfollow popup.
 		setIsActive((prev) => !prev);
-	}
+	};
 	return (
-		<a href='/user' className='user-card-link-wrapper'>
-			<article className='user-card'>
-				<img className='user-card-photo' src={userPhoto} alt='User profile photo' />
-				<div className='user-card-container'>
-					<header className='user-card-header'>
+		<a href="/user" className="user-card-link-wrapper">
+			<article className="user-card">
+				<img className="user-card-photo" src={userPhoto} alt="User profile" />
+				<div className="user-card-container">
+					<header className="user-card-header">
 						<h3>Name</h3>
-						<p className='text-body3'>123 recipes</p>
+						<p className="text-body3">123 recipes</p>
 					</header>
-					<MainButton onClick={onClick} className={isActive ? "unfollow-button" : ""}>
+					<MainButton
+						onClick={onClick}
+						className={isActive ? "unfollow-button" : ""}
+					>
 						{isActive ? "Unfollow" : "Follow"}
 					</MainButton>
 				</div>
-			</article >
+			</article>
 		</a>
-	)
-}
+	);
+};
