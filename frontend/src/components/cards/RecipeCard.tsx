8d6a35264c1f958207de3c6dcadd6c77851fc921
Nick <kolya59264@gmail.com>
2026-03-08 15:49:10 +0200

    Install biome and apply some fixes.

--- frontend/src/components/cards/RecipeCard.tsx
+++ frontend/src/components/cards/RecipeCard.tsx
@@ -1,25 +1,29 @@
-import '../../assets/styles/recipeCard.css'
-import recipeImg from '../../assets/images/vegetable-side-dishes.jpg'
-import { FavoriteButton } from '../buttons/FavoriteButton'
+import "../../assets/styles/recipeCard.css";
+import recipeImg from "../../assets/images/vegetable-side-dishes.jpg";
+import { FavoriteButton } from "../buttons/FavoriteButton";
 
 export const RecipeCard = () => {
 	return (
-		<a href='/recipe-page' className='recipe-card-link-wrapper'>
-			<article className='recipe-card'>
-				<img className='recipe-card-image' src={recipeImg} alt='Vegetable side dishes'></img>
-				<div className='recipe-card-container'>
-					<div className='recipe-card-content-column'>
-						<header className='recipe-card-header'>
-							<h3 className='text-label'>Title</h3>
-							<p className='text-caption-s'>Short description</p>
+		<a href="/recipe-page" className="recipe-card-link-wrapper">
+			<article className="recipe-card">
+				<img
+					className="recipe-card-image"
+					src={recipeImg}
+					alt="Vegetable side dishes"
+				></img>
+				<div className="recipe-card-container">
+					<div className="recipe-card-content-column">
+						<header className="recipe-card-header">
+							<h3 className="text-label">Title</h3>
+							<p className="text-caption-s">Short description</p>
 						</header>
-						<footer className='recipe-card-footer'>
-							<span className='text-caption'>smth</span>
+						<footer className="recipe-card-footer">
+							<span className="text-caption">smth</span>
 							<FavoriteButton />
 						</footer>
 					</div>
 				</div>
 			</article>
 		</a>
-	)
-}
+	);
+};
