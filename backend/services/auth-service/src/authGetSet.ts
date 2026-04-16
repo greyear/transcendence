692e6e48628f422bd89a56583b7db45ff41653e6
Eric Lehtonen <elehtone@student.hive.fi>
2026-04-17 00:38:33 +0300

    mongoDB issue fix. Should now prevent the intermittent "Resource Exists" error on new account creation.

--- backend/services/auth-service/src/authGetSet.ts
+++ backend/services/auth-service/src/authGetSet.ts
@@ -88,7 +88,7 @@ authGetSet.patch(
 			const updatedUser = await userModel.findOneAndUpdate(
 				{ id: userId },
 				{ passwordHash: hashedPassword },
-				{ new: true },
+				{ returnDocument: 'after' },
 			);
 
 			if (updatedUser) {
