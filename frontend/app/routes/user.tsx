4b9baa01a53f6b9b66a4873802f94f911599a3ba
greyear <anna.s.zinchenko@gmail.com>
2026-04-25 22:29:06 +0000

    minor fix

--- frontend/app/routes/user.tsx
+++ frontend/app/routes/user.tsx
@@ -8,7 +8,7 @@ import {
 	useParams,
 } from "react-router";
 import { z } from "zod";
-import userPhoto from "~/assets/images/user-photo.jpg";
+import userPhoto from "~/assets/images/default-avatar.jpeg";
 import "~/assets/styles/userProfile.css";
 import "~/assets/styles/recipesGrid.css";
 import { MainButton } from "~/components/buttons/MainButton";
